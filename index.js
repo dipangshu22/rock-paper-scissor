b972848c978e279a83b0af7ebfa2e6ac815f7474
dipangshu22 <dipangshu22@gmail.com>
2025-07-25 16:11:46 +0530

    third

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let mode = document.getElementById('mode');
 let user_score=0
 let cpu_score=0
 const choices=document.querySelectorAll(".pic")
+const msg=document.querySelector("#msg")
 
 mode.addEventListener('click', function() {
     if (document.body.style.backgroundColor === 'black') {
@@ -9,15 +10,61 @@ mode.addEventListener('click', function() {
         
         mode.innerText = 'dark';
     } else {
-        document.body.style.backgroundColor = 'black';
+       document. body.style.backgroundColor = 'black';
         mode.innerText = 'light';
     }
 });
+const rand=()=>{
+    const option=['rock','paper','scissor']
+    const random=Math.floor(Math.random()*3)
+    return option[random]
+
+}
+
+const drawgame=()=>{
+    msg.innerText="Game is draw 👋"
+    msg.style.backgroundColor = 'grey'
+}
+
+const showwinner=(user,userchoice,cpu_choice)=>{
+    if(user){
+        msg.innerText=`${userchoice} beats ${cpu_choice} : you win!🫡`
+        msg.style.backgroundColor = 'green'
+
+    }
+    else{
+        msg.innerText=`${cpu_choice} beats ${userchoice}  :you loose!🥹`
+        msg.style.backgroundColor = 'red'
+    }
+
+}
+
+const playgame=(userchoice)=>{
+    console.log("user choice",userchoice)
+    const cpu_choice=rand()
+    console.log("cpu choice",cpu_choice)
+    if(userchoice===cpu_choice){
+       return drawgame()
+    }else{
+        let userwin=true;
+        if(userchoice==="rock"){
+            userwin=cpu_choice==="paper"?false:true;
+        }else if(userchoice==="paper"){
+            userwin=cpu_choice==="scissor"?false:true
+        }else{
+            userwin=cpu_choice==="rock"?false:true;
+        }
+        showwinner(userwin,userchoice,cpu_choice)
+        }
+
+    }
 
 choices.forEach((choice)=>{
     choice.addEventListener('click',()=>{
         const choice_id=choice.getAttribute("id")
-        console.log(`${choice_id} was clicked`)
+        // console.log(`${choice_id} was clicked`)
+        playgame(choice_id)
+
     })
 
 })
